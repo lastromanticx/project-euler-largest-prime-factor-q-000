980d6756b056c633eb3b3130e3dee7a0a7422fa5
uu <giladbarkan@live.com>
2016-06-07 23:27:45 -0400

    passed tests

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,26 @@
 # Enter your procedural solution here!
+def largest_prime_factor(n)
+  largest_prime = 1
+  d = 2
+
+  while n > 1
+    if n % d == 0 && d > largest_prime
+      largest_prime = d
+    end
+
+    while n % d == 0
+      n /= d
+    end
+
+    d += 1
+
+    if d * d > n
+      if n > largest_prime
+        largest_prime = n
+      end
+      break
+    end
+  end
+
+  largest_prime
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,32 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(n)
+    @n = n
+  end
+
+  def number
+    largest_prime = 1
+    d = 2
+
+    while @n > 1
+      if @n % d == 0 && d > largest_prime
+        largest_prime = d
+      end
+
+      while @n % d == 0
+        @n /= d
+      end
+
+      d += 1
+
+      if d * d > @n
+        if @n > largest_prime
+          largest_prime = @n
+        end
+        break
+      end
+    end
+
+    largest_prime
+  end
+end
